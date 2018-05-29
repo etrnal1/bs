e4bfa06a54bc3f0821a81e04c12580d168444115
etrnal1 <1026177564@qq.com>
2018-05-29 23:02:37 +0800

    使用中间件 进行了权限控制
    添加了 权限控制

--- bbs/app/Http/Controllers/UserController.php
+++ bbs/app/Http/Controllers/UserController.php
@@ -7,6 +7,11 @@ use App\Models\User;
 use App\Http\Requests\UserRequest;
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -19,15 +24,9 @@ class UserController extends Controller
     public function update(UserRequest $request, User $user)
     {
         
-        echo  '<pre>';
-        var_dump($user);
-
-        echo '</pre>';
-        exit();
+       
         $user->update($request->all());
 
-        var_dump($user);
-        exit();
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
--- bbs/resources/views/layouts/_header.blade.php
+++ bbs/resources/views/layouts/_header.blade.php
@@ -36,7 +36,7 @@
 			<li><a href="{{route('login')}}">登录</a></li>
 			<li><a href="{{route('register')}}">注册</a></li>
 			@else
-			<li class="">
+			<li class="dropdown">
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                                 <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
@@ -47,7 +47,7 @@
 
 			<ul class="dropdown-menu" role="menu">
 
-			
+				<li><a href="{{route('users.edit',Auth::id())}}">编辑资料</a></li>
 				<li>
 					<a href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
 						
