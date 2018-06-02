0108ec69d2d13917d5016b0af97f5d985967b812
etrnal1 <1026177564@qq.com>
2018-06-02 20:41:43 +0800

    修改了发帖编辑器
    添加了发帖编辑器

--- bbs/resources/views/layouts/app.blade.php
+++ bbs/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<title>@yield('title','laravel')-laravel 论坛</title>
 	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+	@yield('styles')
 	<meta charset="utf-8"  http-equiv="x-UA-Compatible" content="IE=edge">
 	<meta charset="utf-8"  name = "viewport" content="width=device-width,initial-scale=1" >
 	<meta charset="utf-8" name="csrf-token" content="{{csrf_token()}}">
@@ -18,5 +19,6 @@
 		@include('layouts._footer')
 	</div>
 	 <script src="{{ asset('../js/app.js') }}"></script>
+	 @yield('scripts')
 </body>
 </html>
