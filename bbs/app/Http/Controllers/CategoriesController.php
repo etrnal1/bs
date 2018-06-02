931f3e5b93b56935c70ac063995c8231e90dbcb6
etrnal1 <1026177564@qq.com>
2018-06-02 19:17:24 +0800

    话题列表完成
    完善了最新回复 与最新发布

--- bbs/app/Http/Controllers/CategoriesController.php
+++ bbs/app/Http/Controllers/CategoriesController.php
@@ -9,10 +9,12 @@ use App\Models\Category;
 class CategoriesController extends Controller
 {
 
-	 public function show(Category $category)
+	 public function show(Category $category,Request $request ,Topic $topic)
 	 {
   //读取分类ID关联的话题
-	 	$topics = Topic::where('category_id',$category->id)->paginate(28);
+	 	$topics = $topic->withOrder($request->order)
+	 	->where('category_id',$category->id)
+	 	->paginate(20);
 	 	//传递参数到模板
 	 	return view('topics.index',compact('topics','category'));
 
--- bbs/app/Models/Topic.php
+++ bbs/app/Models/Topic.php
@@ -5,6 +5,41 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+
+/**
+ * [scopeWithOrder description]不同的排序，使用不同的数据读取逻辑
+ * @param  [type] $query [description]
+ * @param  [type] $order [description]
+ * @return [type]        [description]
+ */
+    public function  scopeWithOrder($query,$order)
+    {
+    		
+    		  switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+
+    }
+     public  function  scopeRecentReplied($query)
+     {
+
+     	//当话题有新回复时,我们将编写逻辑更新话题模型的reply_count 模型
+     	return $query->orderby('created_at','desc');
+     }
+
+
+
+
+
     public function category()
     {
     	return  $this->belongsTo(Category::class);
